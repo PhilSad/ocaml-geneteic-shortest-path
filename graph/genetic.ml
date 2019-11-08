1cfa181e304c5f40ae82c12d32a4cff233d068a5
PhilSad <philippe.henri.saade@gmail.com>
2019-11-08 03:33:14 +0100

    fonction shuffle liste

--- graph/genetic.ml
+++ graph/genetic.ml
@@ -76,86 +76,77 @@ let selection pop perct_elit=
   let sort_fun = fun p1 p2 -> if sub p1.fit p2.fit > 0. then -1 else 1 in
   let ranked_pop = List.sort sort_fun pop in
 
+  (* Selection elitiste : on garde les perct_elit% meilleur *)
   let rec get_elits ranked_pop nb = match (nb, ranked_pop) with
     | 0,_  -> []
     | _,[] -> []
     | nb, head :: rest -> head :: get_elits rest (nb-1)
   in
 
-  (* pourcentage à garder -> nb à garder *)
+  (* Selection par roulette *)
+  let rec get_roulette ranked_pop nb = match nb with
+    | 0 -> []
+    | nb ->
+
+        let rand = float_of_int (Random.int 101) in
+        let tot_fitness = List.fold_left (fun acc indiv -> add indiv.fit acc) 0. pop in
+        let roul = div (mul rand tot_fitness) 100.  in
+        let rec find_gagnant ranked_pop roul sumacc = match ranked_pop with
+          | [] -> failwith "impossible de trouver un gagnant"
+          | {chem = c ; fit = f} :: rest ->
+              print_list ranked_pop;
+              if add sumacc f < roul 
+              then find_gagnant rest roul (add sumacc f )
+              else 
+                {chem = c ; fit = f}
+        in
+
+          find_gagnant ranked_pop roul 0. :: get_roulette ranked_pop (nb-1)
+  in
+
   let nb_elits = perc_to_nb (List.length pop) perct_elit in
+  let nb_roulette = int_of_float (div (float_of_int (List.length pop)) 2. ) - nb_elits in
 
-    Printf.printf "%d\n" nb_elits;
+  let elits = get_elits ranked_pop nb_elits in
+  let gagnants = get_roulette ranked_pop nb_roulette in
 
-    let nb_roulette = List.length pop - nb_elits in
-
-    let rec get_roulette ranked_pop nb = match (nb, ranked_pop) with
-      | _, [] -> []
-      | 0, _  -> []
-      | nb, ranked_pop ->
-
-          let rand = float_of_int (Random.int 101) in
-          let tot_fitness = List.fold_left (fun acc indiv -> add indiv.fit acc) 0. pop in
-            Printf.printf "tot_fit = %f\n" tot_fitness;
-            let roul = div (mul rand tot_fitness) 100.  in
-              Printf.printf "%f\n" roul;
-              let rec find_gagnant ranked_pop roul sumacc = match ranked_pop with
-                | [] -> failwith "impossible de trouver un gagnant"
-                | {chem = c ; fit = f} :: rest ->
-                    Printf.printf "fit = %f sumacc = %f\n" f (add sumacc f);
-                    print_list ranked_pop;
-                    if add sumacc f < roul 
-                    then find_gagnant rest roul (add sumacc f )
-                    else 
-                      {chem = c ; fit = f}
-              in
-
-                find_gagnant ranked_pop roul 0. :: get_roulette ranked_pop (nb-1)
-    in
-
-    let elits = get_elits ranked_pop nb_elits in
-    let gagnants = get_roulette ranked_pop nb_roulette in
-
-      List.append elits gagnants;;
+    List.append elits gagnants;;
 
 
 let rec print_list = function
   | [] -> Printf.printf "\n%!"
   | {chem = c ; fit = f} :: rest -> Printf.printf "%f |" f ; print_list rest;;
 
+let pop = [{chem = []; fit = 3.};{chem = []; fit = 1.};{chem = []; fit = 2.};{chem = []; fit = 1.};{chem = []; fit = 1.};{chem = []; fit = 1.};{chem = []; fit = 1.};{chem = []; fit = 1.}];;
 print_list  pop;;
-
-let pop = [{chem = []; fit = 3.};{chem = []; fit = 1.}];;
-
-List.fold_left (fun acc indiv -> add indiv.fit acc) 0. pop;;
-
-pop;;
-
-selection pop 50.;;
+selection pop 0.;;
 
 
-let pop = graphs_to_indivs (n_random_path graph 10);;
-
-selection pop 25.;;
-
-perc_to_nb 100 25.;;
 
 
-let tot_fitness = List.fold_left (fun acc indiv -> 0 indiv.fit + acc) 0 pop
-
-let elits = get_elits ranked_pop 2 in
-  elits;;
-
-1.5+ 2.5;;
 
 
-let tot_fitness = List.fold_left (fun acc indiv -> add indiv.fit acc) 0. pop;;
+(* CROSSOVER *)
 
-2.0 > 1.0;;
 
-Random.int 101;;
+let shuffle l =
+  let rand = List.map (fun e -> (Random.bits (), e ) ) l in
+  let sorted_rand = List.sort (fun a b -> fst a - fst b)  rand in
+    List.map snd sorted_rand;;
 
-int_of_float 2.9
+let a = [1;2;3;4;5;6;7];;
+shuffle a;;
+
+
+let rec cross_over pop =  
+
+
+
+
+
+
+
+
 
 
 
