b957ea3a4d114def8a4a49c34db5e55d51fefe56
PhilSad <philippe.henri.saade@gmail.com>
2019-11-08 01:38:37 +0100

    selection elitisme

--- graph/genetic.ml
+++ graph/genetic.ml
@@ -30,7 +30,7 @@ Float.sub 2.5 1.5;;
 
 sqrt 9.**2;;
 
-(* Fitness function *)
+(* Fitness function = distance entre les noeuds de la liste*)
 
 let rec fitness (gr:Graph2.graph) = match gr with
   | [] -> 0.
@@ -44,7 +44,81 @@ let rec fitness (gr:Graph2.graph) = match gr with
 let p = random_path graph;;
 
 fitness p;;
+graph;;
 
-p;;
 
-print_string "aaa"
+type individu = {chem : Graph2.graph; fit : float};;
+
+let graph_to_indiv gr = {chem = gr; fit = fitness gr};; 
+let rec graphs_to_indivs pop = List.map graph_to_indiv pop;;
+
+let pop = n_random_path graph 5;;
+
+graphs_to_indivs pop;;
+
+graph_to_indiv graph;;
+
+div 2.0  5.6;;
+
+
+let nb_elits = int_of_float (div (mul 50 List.length pop) 100. ) 
+
+
+open Float;;
+
+(* SELECTION : roullette wheel + elitisme *)
+
+let selection pop =
+  let sort_fun = fun p1 p2 -> if sub p1.fit p2.fit > 0. then -1 else 1 in
+  let ranked_pop = List.sort sort_fun pop in
+  let rec get_elits ranked_pop nb = match (nb, ranked_pop) with
+    | 0,_  -> []
+    | _,[] -> []
+    | nb, head :: rest -> head :: get_elits rest (nb-1)
+  in
+
+  let nb_elits = int_of_float (div (mul perct_elit List.length pop) 100. ) 
+                               let elits = get_elits ranked_pop 2 in
+                                 elits;;
+
+                               ;;
+
+                               let pop = graphs_to_indivs (n_random_path graph 5);;
+
+                               selection pop;;
+
+
+                               2.0 > 1.0;;
+
+
+
+                               int_of_float 2.9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
